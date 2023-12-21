1de223a27689db4ba271e8eaef0584cb286af2a0
Johnny Jorges Vega <126032977+johnnyjorgesvega@users.noreply.github.com>
2023-12-21 20:00:08 +0000

    "primer_cambio"

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    // Configurar el intervalo de cambio de slide cada 4 segundos (4000 milisegundos)
+    $('#carouselExampleIndicators').carousel({
+      interval: 4000
+    });
+  });
