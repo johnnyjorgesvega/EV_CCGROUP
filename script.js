3adb1eb56aabc8f1591e601543795251ffb22be6
Johnny Jorges Vega <126032977+johnnyjorgesvega@users.noreply.github.com>
2023-12-27 13:48:30 +0000

    "27/12 Cambio"

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    // Configurar el intervalo de cambio de slide cada 4 segundos (4000 milisegundos)
     $('#carouselExampleIndicators').carousel({
       interval: 4000
     });
